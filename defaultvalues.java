83f00709054cce778ae04c4a6c1359ffa9778981
Chandini-R <161247008+Chandini-R@users.noreply.github.com>
2024-02-28 22:24:40 +0530

    Add files via upload

---
+++ defaultvalues.java
@@ -0,0 +1,36 @@
+package week2;
+class values
+{
+	byte Byte;
+	int integer;
+	long Long;
+	short Short;
+	boolean Boolean;
+	String string;
+	float Float;
+	char ch;
+	double Double;
+	void getvalues()
+	{
+		System.out.println("The Default value of primitive datatype Byte:" +Byte);
+		System.out.println("The Default value of primitive datatype integer:" +integer);
+		System.out.println("The Default value of primitive datatype Long:" +Long);
+		System.out.println("The Default value of primitive datatype Short:" +Short);
+		System.out.println("The Default value of primitive datatype Boolean:" +Boolean);
+		System.out.println("The Default value of primitive datatype string:" +string);
+		System.out.println("The Default value of primitive datatype Float:" +Float);
+		System.out.println("The Default value of primitive datatype char:" +ch);
+		System.out.println("The Default value of primitive datatype double:" +Double);		
+	}
+}
+	
+
+public class defaultvalues {
+
+	public static void main(String[] args) {
+		values var=new values();
+		var.getvalues();
+
+	}
+
+}
